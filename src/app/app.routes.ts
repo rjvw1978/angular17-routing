6c6571e6590d8c71bee47f5a7715305ac5c47a3e
Rojas Córsico Ivana Soledad <irojas@gmail.com>
2024-04-21 17:56:21 -0300

    home route

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -4,9 +4,11 @@ import { RegistroComponent } from './pages/auth/registro/registro.component';
 import { QuienesSomosComponent } from './pages/quienes-somos/quienes-somos.component';
 import { ServiciosComponent } from './pages/servicios/servicios.component';
 import { DashboardComponent } from './pages/dashboard/dashboard.component';
+import { HomeComponent } from './pages/home/home.component';
 
 export const routes: Routes = [
-    {path: "home", component:DashboardComponent},
+    {path: "home", component:HomeComponent},
+    {path:"panel-control", component:DashboardComponent}, 
     {path:"iniciar-sesion", component:IniciarSesionComponent},
     {path:"registro", component:RegistroComponent},
     {path:"quienes-somos", component:QuienesSomosComponent},
